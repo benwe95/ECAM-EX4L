e78adcbb6c9baf7f781e8a196ab891960fa9d061
manoelledeviron <manoelle.deviron@skynet.be>
2017-12-21 21:06:33 +0100

    added c file ListFiles.c

---
+++ ListFiles.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+
+//Open a directory and get the list of files/directories inside
+// @dir[]: give the name of the directory
+// @extension[]: give the extension you're looking for (txt, docx, py, c, ...)
+void openAndGet(char dir[],char extension[]){
+  struct dirent *pDirent;
+  DIR *pDir;
+  char *p;
+  pDir = opendir (dir);
+  // test if there is a dir with that name
+  if (pDir == NULL) {
+   printf ("Cannot open directory '%s'\n", dir);
+  }
+  else{
+
+    //Read what's inside of the pDir
+    while ((pDirent = readdir(pDir)) != NULL) {
+      struct stat st;
+      lstat(pDirent->d_name, &st);
+      int isDir = 0;
+      // get name of the file/directory
+      // just links to previous directories? (. or ..)
+      if (strncmp(pDirent->d_name,".",100)==0 || strncmp(pDirent->d_name,"..",100)==0){
+        //
+      }
+      else
+      {
+        //get position of the last "." and then read from after the point to the end
+        //char *initialdot = strtok(pDirent->d_name,".");
+        char *dot = strtok(pDirent->d_name,".");
+        while (dot != NULL){
+          p = dot;
+          dot = strtok(NULL,".");
+        }
+        if (p == pDirent->d_name){
+          isDir = 1;
+          char firstpath[100], middlepath[5], lastpath[100];
+          strcpy(firstpath,dir);
+          strcpy(middlepath,"/");
+          strcpy(lastpath,pDirent->d_name);
+          strcat(firstpath,middlepath);
+          strcat(firstpath,lastpath);
+          openAndGet(firstpath,extension);
+        }
+        if (strncmp(extension,"all",50)==0){
+          printf("%s/%s",dir,pDirent->d_name);
+          if (isDir == 0 && strncmp(".DS_Store",pDirent->d_name,50)!=0)
+            printf(".%s\n",p);
+          else
+            printf("\n");
+        }
+        else if (strncmp(p,extension,50)==0){
+          printf("%s/%s.%s\n",dir,pDirent->d_name,extension);
+        }
+      }
+    }
+    // close dir pDir
+    if (pDir != NULL){
+      closedir(pDir);
+      pDir = NULL;
+    }
+  }
+
+}
+
+int main (int c, char *v[]) {
+   int len;
+   struct dirent *pDirent;
+   struct dirent *newDirent;
+   DIR *pDir;
+   DIR *newDir;
+   char *p;
+   char *q;
+   printf("c: %d\n",c);
+   struct stat fileStat;
+   char buf[PATH_MAX + 1];
+
+   if (c < 2) {
+       printf ("Usage: ./test2 <dirname> <ext>\n");
+       return 1;
+   }
+   printf("v[0]: %s\n", v[0]);
+   printf("v[1]: %s\n", v[1]);
+   printf("v[2]: %s\n", v[2]);
+
+   openAndGet(v[1],v[2]);
+
+   return 0;
+}
